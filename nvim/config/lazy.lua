6456457c1c09ce669be0bf01b5751f962d8a9085
yuniruyuni <yuniruyuni@gmail.com>
2026-03-28 18:41:27 +0900

    nvim/config/lazy.lua のプラグインに設定を追加する

--- nvim/config/lazy.lua
+++ nvim/config/lazy.lua
@@ -28,14 +28,60 @@ require("lazy").setup({
     { "rhysd/clever-f.vim" },
     { "unblevable/quick-scope" },
     { "wellle/targets.vim" },
-    { "monaqa/dial.nvim" },
-    { "rapan931/lasterisk.nvim" },
-    { "kevinhwang91/nvim-hlslens" },
-    { "gbprod/substitute.nvim" },
+    {
+      "monaqa/dial.nvim",
+      keys = {
+        { "<C-a>", function() require("dial.map").manipulate("increment", "normal") end, desc = "Increment" },
+        { "<C-x>", function() require("dial.map").manipulate("decrement", "normal") end, desc = "Decrement" },
+        { "g<C-a>", function() require("dial.map").manipulate("increment", "gnormal") end, desc = "Increment (g)" },
+        { "g<C-x>", function() require("dial.map").manipulate("decrement", "gnormal") end, desc = "Decrement (g)" },
+        { "<C-a>", function() require("dial.map").manipulate("increment", "visual") end, mode = "v", desc = "Increment" },
+        { "<C-x>", function() require("dial.map").manipulate("decrement", "visual") end, mode = "v", desc = "Decrement" },
+        { "g<C-a>", function() require("dial.map").manipulate("increment", "gvisual") end, mode = "v", desc = "Increment (g)" },
+        { "g<C-x>", function() require("dial.map").manipulate("decrement", "gvisual") end, mode = "v", desc = "Decrement (g)" },
+      },
+    },
+    {
+      "rapan931/lasterisk.nvim",
+      keys = {
+        { "*", function() require("lasterisk").search() end, desc = "Search word under cursor" },
+        { "g*", function() require("lasterisk").search({ is_whole = false }) end, desc = "Search partial word" },
+        { "*", function() require("lasterisk").search({ is_whole = false }) end, mode = "x", desc = "Search visual selection" },
+      },
+    },
+    {
+      "kevinhwang91/nvim-hlslens",
+      config = function()
+        require("hlslens").setup()
+      end,
+    },
+    {
+      "gbprod/substitute.nvim",
+      config = function()
+        require("substitute").setup()
+      end,
+      keys = {
+        { "s", function() require("substitute").operator() end, desc = "Substitute operator" },
+        { "ss", function() require("substitute").line() end, desc = "Substitute line" },
+        { "S", function() require("substitute").eol() end, desc = "Substitute to EOL" },
+        { "s", function() require("substitute").visual() end, mode = "x", desc = "Substitute visual" },
+      },
+    },
     { "haya14busa/vim-edgemotion" },
     { "haya14busa/vim-metarepeat" },
-    { "keaising/im-select.nvim" },
-    { "kylechui/nvim-surround" },
+    {
+      "keaising/im-select.nvim",
+      config = function()
+        require("im_select").setup({})
+      end,
+    },
+    {
+      "kylechui/nvim-surround",
+      event = "VeryLazy",
+      config = function()
+        require("nvim-surround").setup({})
+      end,
+    },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
